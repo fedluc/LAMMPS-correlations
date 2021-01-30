e9135a4c5b41655f923d076871c5ef6160649dfa
fedluc <flc@kth.se>
2021-01-30 19:45:57 +0100

    Fixed private variables during parallel computation, added lvcf calculation

--- lmp_corr.h
+++ lmp_corr.h
@@ -16,9 +16,15 @@ typedef struct {
 
 } input;
 
+// -------------------------------------------------------------------
+// CALLER FOR THE OTHER FUNCTIONS IN THE FILE
+// -------------------------------------------------------------------
+
 void analyze_lmp(input in);
 
-void get_file_names(char *file_pattern);
+// -------------------------------------------------------------------
+// FUNCTIONS USED TO COMPUTE THE INTERMEDIATE SCATTERING FUNCTION
+// -------------------------------------------------------------------
 
 void isf();
 
@@ -37,11 +43,43 @@ void isf_free(double *out_sim_box, double *xx, double *yy, double *zz,
 	      double *qq, double complex *drhok, double complex *drhomk,
 	      double complex *fkt);
 
+
+// -------------------------------------------------------------------
+// FUNCTIONS USED TO COMPUTE THE LONGITUDINAL VELOCITY CORRELATIONS
+// -------------------------------------------------------------------
+
 void lvcf();
+
+void lvcf_init(int *out_n_atoms, double *out_LL, double *out_dq, int *out_nq,
+              int *out_nq_dir, double **out_sim_box,
+              double **out_xx, double **out_yy, double **out_zz,
+              double **out_vx, double **out_vy, double **out_vz,
+              double **out_qq, double complex **out_dvk,
+              double complex **out_dvmk, double complex **out_ckt);
+
+void lvcf_output(double complex *ckt, double dq, int nq, int n_files);
+
+void lvcf_free(double *out_sim_box, double *xx, double *yy, double *zz,
+	      double *vx, double *vy, double *vz,
+	      double *qq, double complex *dvk, double complex *dvmk,
+	      double complex *ckt);
+
+
+// -------------------------------------------------------------------
+// FUNCTIONS USED TO ACCESS THE MULTIDIMENSIONAL ARRAYS
+// -------------------------------------------------------------------
+
 
 int idx2(int xx, int yy, int x_size);
 
 int idx3(int xx, int yy, int zz, int x_size, int y_size);
+
+
+// -------------------------------------------------------------------
+// FUNCTIONS USED TO READ LAMMPS OUTPUT
+// -------------------------------------------------------------------
+
+void get_file_names(char *file_pattern);
 
 void read_file_init(int *num_atoms, double *min_box_size);
 
